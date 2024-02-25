c13a99ba98a0183bedc635576dc95ce3bfb842ba
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-02-25 20:13:45 +0100

    consultas

--- Actividad3_Alberto_V2/src/main/java/prueba/_01_MostrarLibros.java
+++ Actividad3_Alberto_V2/src/main/java/prueba/_01_MostrarLibros.java
@@ -12,31 +12,32 @@ public class _01_MostrarLibros {
 
 	public static void main(String[] args) {
 		
-		 EntityManagerFactory emf = Persistence.createEntityManagerFactory("tuUnidadDePersistencia");
-	        EntityManager em = emf.createEntityManager();
+	 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Actividad3JPA");
+        EntityManager em = emf.createEntityManager();
 
-	        // ... (código para dar de alta autores, editoriales y libros)
+        // ... (código para dar de alta autores, editoriales y libros)
 
-	        // Mostrar todos los libros con su editorial y autor
-	        em.getTransaction().begin();
-	        
-	        
-	        //Mostrar todos los libros dados de alta, con su editorial y su autor
-	        
-	        Query query = em.createQuery("Select l from Libros l");
-	        List<Libro> libros = query.getResultList();
+        // Mostrar todos los libros con su editorial y autor
+        em.getTransaction().begin();
+        
+        
+        //Mostrar todos los libros dados de alta, con su editorial y su autor
+        
+        Query query = em.createQuery("Select l from Libro l");
+        List<Libro> libros = query.getResultList();
 
-	        for (Libro libro : libros) {
-	            System.out.println("Libro: " + libro.getTitulo());
-	            System.out.println("Editorial: " + libro.getEditorial().getNombre());
-	            System.out.println("Autor: " + libro.getAutor().getNombre() + " " + libro.getAutor().getApellidos());
-	            System.out.println("--------------------");
-	        }
+        for (Libro libro : libros) {
+        	System.out.println("----libros dados de alta, con su editorial y su autor----");
+            System.out.println("Libro: " + libro.getTitulo());
+            System.out.println("Editorial: " + libro.getEditorial().getNombre());
+            System.out.println("Autor: " + libro.getAutor().getNombre() + " " + libro.getAutor().getApellidos());
+            System.out.println("---------------------------------------------------------");
+        }
 
-	        //em.getTransaction().commit();
+        //em.getTransaction().commit();
 
-	        em.close();
-	        emf.close();
+        em.close();
+        emf.close();
 
 	}
 
---
+++ Actividad3_Alberto_V2/src/main/java/prueba/_04_LibrosLibreria.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class _04_LibrosLibreria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
